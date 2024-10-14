3a53b568adb17c3f2912ffddfd8f8932eb6e9b34
Roman77B <rbodrov@gmail.com>
2024-10-14 07:13:53 +0300

    Home Work if, elif, else

--- module_3_4.py
+++ module_3_4.py
@@ -14,9 +14,7 @@ def single_root_words(root_word, *other_words):
     for w in other_words:
         # 4. Пропишите корректное относительно задачи условие,
         # при котором добавляются слова в результирующий список same_words.
-        if root_word in w:
-            same_words.append(w)
-        if w.lower() in root_word.lower():
+        if root_word in w or w.lower() in root_word.lower():
             same_words.append(w)
     # 5. После цикла верните образованный функцией список same_words.
     return same_words
