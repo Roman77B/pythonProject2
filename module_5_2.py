0c22135e04e8d79a7293669327f968c0c9adc2d1
Roman77B <rbodrov@gmail.com>
2024-11-03 10:27:38 +0300

    Home Work if, elif, else

--- module_5_2.py
+++ module_5_2.py
@@ -19,17 +19,25 @@
 # 10
 # 20
 
+# Создайте новый класс House
+# Создайте инициализатор для класса House, который будет задавать атрибут этажности self.numberOfFloors = 0
+# Создайте метод setNewNumberOfFloors(floors), который будет изменять атрибут numberOfFloors на параметр floors и выводить в консоль numberOfFloors
+# Полученный код напишите в ответ к домашнему заданию
+
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def setNewNumberOfFloors(self, floors):
+        self.number_of_floors = floors  # number_of_floors
+        print (self.number_of_floors)
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __len__(self):
         return self.number_of_floors
-
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
